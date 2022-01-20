2ea582f475da4ed5fc258ee066fa5cbc0d40b350
jooheek <55754373+jooheek@users.noreply.github.com>
2022-01-20 09:37:03 +0900

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//navbar 가 위에 있을 때 투명하게 만든다.
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
