d1a47dde39a1354f5fab11b1d90b4ec153048bc0
jooheek <55754373+jooheek@users.noreply.github.com>
2022-01-20 11:31:58 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//navbar 가 위에 있을 때 투명하게 만든다.
+//navbar 가 위에 있을 때 투명하게 만듦
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -31,6 +31,15 @@ contactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
+//하단으로 스크롤시 home 투명도 증가
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//selector 선택시 스크롤되는 공통 함수
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
